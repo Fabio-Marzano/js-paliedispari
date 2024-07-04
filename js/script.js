00d2f8afbc05f601bd1b3451aba077e86e09588b
fabiomarzano <fabiomarzano1993@gmail.com>
2024-07-05 01:24:51 +0200

    [UPD] create-variable-for-permitted-insert-name

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//CONSENTIAMO L'INSERIMENTO DELLA PAROLA DA PARTE DELL'UTENTE//
+let word = prompt('Inserisci la parola'); 
+//PRENDO LA PAROLA INSERITA DALL'UTENTE LA SUDDIVIDO E NE INVERTO L'ORDINE//
+let check_pal = word.split('').reverse().join('');
+
+
