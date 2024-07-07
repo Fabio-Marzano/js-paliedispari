669bc4497156b9573488acd798a2bb28a1d58463
fabiomarzano <fabiomarzano1993@gmail.com>
2024-07-07 14:30:40 +0200

    [UPD] generate-prompt-for-insert-word-and-number

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
 //PALINDROMA//
 
 //CONSENTIAMO L'INSERIMENTO DELLA PAROLA DA PARTE DELL'UTENTE//
-let word = prompt('Inserisci la parola'); 
+//let word = prompt('Inserisci la parola'); 
 //PRENDO LA PAROLA INSERITA DALL'UTENTE LA SUDDIVIDO E NE INVERTO L'ORDINE//
-let check_pal = word.split('').reverse().join('');
+//let check_pal = word.split('').reverse().join('');
 
-function countWords (string){
+//function countWords (string){
 
-    for( let i = 0; i < string.length; i++){
+   // for( let i = 0; i < string.length; i++){
 
-        if(word == check_pal){
-            console.log('La stringa e palindroma') //CONTROLLO SE LA PAROLA INSERITA E' PALINDROMA
-        }else {
-            console.log('La stringa non e palindroma')  //SE LA PAROLA INSERITA NON E PALINDROMA
-            return'la stronga non e palindroma'  //ESCO DAlLA FUNZIONE QUANDO RILEVATO IL FATTO
-        }
+        //if(word == check_pal){
+           // console.log('La stringa e palindroma') //CONTROLLO SE LA PAROLA INSERITA E' PALINDROMA
+       // }else {
+          //  console.log('La stringa non e palindroma')  //SE LA PAROLA INSERITA NON E PALINDROMA
+           // return'la stronga non e palindroma'  //ESCO DAlLA FUNZIONE QUANDO RILEVATO IL FATTO
+        //}
         
         
              
-    }
-    return('La stringa non e palindroma');
+   // }
+   // return('La stringa non e palindroma');
     
     
-}
+//}
 
-let wordpal = countWords(word);
+//let wordpal = countWords(word);
 
 //PARI E DISPARI//
 
@@ -44,3 +44,11 @@ function checkEvenOrOdd() {
     return 'dispari';
 }
 
+    //DICHIARO LA VARIABILE CON IL PROMPT//
+    let even_or_odd = prompt('Inserisci la parola "pari" o "dispari"');
+
+    let number = prompt('Inserisci un valore compreso tra 1 e 5');
+
+    let pc_number = generateRandomNumber();
+
+    let sum = number + pc_number;
